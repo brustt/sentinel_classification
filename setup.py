b6acb69b1646ce6a0769e935bbc562eb0c6d5f43
rustt <martin.dzr@protonmail.com>
2023-12-26 16:20:57 +0100

    init commit ccds template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='processing S1 and S2 data and simple land use classification',
+    author='martin_dzr',
+    license='MIT',
+)
