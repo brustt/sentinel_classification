eb16b62014ba1e8f55109735a9da69a1de241df2
rustt <martin.dzr@protonmail.com>
2024-01-22 08:33:28 +0100

    s1 pipeline processing

--- src/config.py
+++ src/config.py
@@ -3,12 +3,32 @@ from pathlib import Path
 from dotenv import find_dotenv
 
 
+root_path = os.path.dirname(find_dotenv())
+root_data_path = str(Path(root_path) / "data")
+
+data_dir_raw = str(Path(root_data_path) / "raw")
+data_dir_processed = str(Path(root_data_path) / "processed")
+data_dir_final = str(Path(root_data_path) / "final")
+
+
+study_area = str(Path(data_dir_raw) / "StudyArea/StudyArea.shp")
+RAM = 1024
+CRS = 32622
+
 class ConfigS1:
-    root_path = os.path.dirname(find_dotenv())
-    prod_path = str(Path(root_path) / "data/raw/s1/S1B_IW_GRDH_1SDV_20170425T214234_20170425T214302_005323_00953E_F02D.SAFE")
-    prod_path_2 = str(Path(root_path) / "data/raw/s1/S1B_IW_GRDH_1SDV_20170706T214238_20170706T214306_006373_00B331_CEE0.SAFE")
-    area_path = str(Path(root_path) / "data/raw/area/StudyArea.shp")
-    crs = 32622 # or 4326
     
-    S1_EXTENSION_FILE = (".tiff", ".tif")
+    s1_dir = str(Path(data_dir_raw) / "S1/zip")
+    geoid_path = str(Path(data_dir_raw) / "Geoid" / "egm96.grd")
+    dem_path = str(Path(data_dir_raw) / "Tests" / "DEM" / "srtm_1secarc_Cayenne.tif")
+    # raster fo test
+    raster_path_tif = str(Path(s1_dir) / "S1B_IW_GRDH_1SDV_20170425T214234_20170425T214302_005323_00953E_F02D.SAFE/measurement/s1b-iw-grd-vv-20170425t214234-20170425t214302-005323-00953e-001.tiff")
+
+    SENTINEL1_NAMESPACES = {
+        "safe": "http://www.esa.int/safe/sentinel-1.0",
+        "s1": "http://www.esa.int/safe/sentinel-1.0/sentinel-1",
+        "s1sarl1": "http://www.esa.int/safe/sentinel-1.0/sentinel-1/sar/level-1",
+    }
     
+    calib_type = "sigma"
+    
+    meta_product_path = str(Path(data_dir_processed) / "S1" / "s1_products_meta.csv")
